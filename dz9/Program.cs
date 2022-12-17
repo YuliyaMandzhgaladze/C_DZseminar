494a92bd5f9788f5cb4ce4841dd6b404879719a0
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 16:04:22 +0400

    dz9готово

---
+++ dz9/Program.cs
@@ -0,0 +1,15 @@
+﻿// 9. Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+int A = 24;
+Console.Write("Введите число: ");
+int B = int.Parse(Console.ReadLine());
+if(A % B == 0)
+{
+    Console.Write("Число кратно заданному");
+}
+else
+{
+    Console.WriteLine("Число НЕ кратно заданному");
+    Console.Write("Остаток: ");
+    Console.WriteLine(A % B);
+}
