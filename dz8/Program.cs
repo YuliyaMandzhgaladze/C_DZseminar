e3a1a66408a659e61bd0a47eb6ebfd6655407d9d
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 16:20:12 +0400

    dz8готово

---
+++ dz8/Program.cs
@@ -0,0 +1,9 @@
+﻿// 8. Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine());
+int B = A / 100;
+int C = A % 10;
+Console.Write(B);
+Console.Write(C);
+
