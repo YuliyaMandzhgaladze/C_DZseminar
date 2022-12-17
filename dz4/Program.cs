62ea0357700e4a8f45762fed3a5c5f85773c5817
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 15:23:50 +0400

    dz4готово

---
+++ dz4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+if (N % 2 == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число не четное");
+}
