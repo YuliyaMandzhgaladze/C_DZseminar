0be26bdf03f873cd481671846c06f481f8bf44d9
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 15:36:39 +0400

    dz5готово

---
+++ dz5/Program.cs
@@ -0,0 +1,7 @@
+﻿// 5. Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine());
+int B = A % 10;
+Console.Write("Последняя цифра числа: ");
+Console.WriteLine(B);
