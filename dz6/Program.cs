13e3a04ecb5281fa1d0c15a659c119557126ddc8
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 15:43:26 +0400

    dz6готово

---
+++ dz6/Program.cs
@@ -0,0 +1,7 @@
+﻿// 6. Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine());
+int B = A / 10 % 10;
+Console.Write("Вторая цифра числа: ");
+Console.WriteLine(B);
