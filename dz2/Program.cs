374c8b751194d72ab847f99fb739b59de74868c1
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 14:55:50 +0400

    dz2готово

---
+++ dz2/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите число дня недели: ");
+int D = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+if (D == a)
+{
+    Console.Write("Понедельник");
+}
+if (D == b)
+{
+    Console.Write("Вторник");
+}
+if (D == c)
+{
+    Console.Write("Среда");
+}
+if (D == d)
+{
+    Console.Write("Четверг");
+}
+if (D == e)
+{
+    Console.Write("Пятница");
+}
+if (D == f)
+{
+    Console.Write("Суббота");
+}
+if (D == g)
+{
+    Console.Write("Воскресенье");
+}
+else
+{
+    Console.Write("Только число от 1 до 7: ");
+}
