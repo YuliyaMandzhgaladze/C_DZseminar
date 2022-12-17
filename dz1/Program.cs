641c2fb711120fb5593a00563c32b8b17ed290f6
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-17 14:03:12 +0400

    dz1готово

---
+++ dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a == b*b)
+{
+    Console.Write("Первое число квадрат второго");
+}
+
+else
+{
+    Console.WriteLine("Первое число НЕ квадрат второго");
+}
