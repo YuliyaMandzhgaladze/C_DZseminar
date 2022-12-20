52957ba5e1e4a9f15d9e26e29114738fe421e8c8
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-20 13:21:38 +0400

    заметки

---
+++ zametki/Program.cs
@@ -0,0 +1,21 @@
+﻿// преобразование числа в строку
+
+//int number = int.Parse(Console.ReadLine()!);
+//string numStr = $"{number}";
+//int ln = numStr.Length;
+
+//int N = int.Parse(Console.ReadLine()!);
+//string str = $"{N}";
+
+// пример поиска третьей цифры:
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+if (num.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " + num[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
