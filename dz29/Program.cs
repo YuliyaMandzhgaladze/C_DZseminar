0c7163f359d2ffc18ef2b9dd0b45c77b3f0cfe61
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-21 20:28:23 +0400

    dz29готово

---
+++ dz29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 случайных чисел (-10, 10) и выводит их на экран.
+//
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+void NArray(int[] masN)
+{
+    
+    int length = masN.Length;
+    int i = 0;
+    while (i < length)
+    {
+        masN[i] = new Random().Next(-10, 10);
+        i++;
+    }
+}
+
+void PArray(int[] masP)
+{
+    int count = masP.Length;
+    int i1 = 0;
+    while (i1 < count)
+    {
+        Console.WriteLine(masP[i1]);
+        i1++;
+    }
+}
+
+int[] array = new int[8];
+
+NArray(array);
+PArray(array);
