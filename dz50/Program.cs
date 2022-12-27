765a4d758d57a5e6c9137c6b51c5e2276f2a2a1c
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-27 21:07:19 +0400

    dz50

---
+++ dz50/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число,
+//  и возвращает индексы числа в двумерном массиве или же указание, 
+//  что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+void FillArray (int[, ]array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArray (int[, ]array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int [, ] arrayDone = new int [3, 4];
+FillArray(arrayDone);
+//Console.WriteLine();
+PrintArray(arrayDone);
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < arrayDone.GetLength(0); i++)
+{
+    for (int j = 0; j < arrayDone.GetLength(1); j++)
+    {
+        if (number == arrayDone[i, j])
+        {
+            Console.WriteLine("Индексы числа: "+i+", "+j);
+          
+            return;
+
+        }
+        if (number > arrayDone[i, j]&& number < arrayDone[i, j])
+            {
+                Console.Write("нет");
+                return;
+            }
+        
+
+        
+
+    }
+}
+
+
+
+
+
+
+   
